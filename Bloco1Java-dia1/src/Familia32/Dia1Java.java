1e271947c4a1264efbf695bc4e2927b71c7e9cee
Gustavo Henrique da Silva <gushsilva2013@gmail.com>
2021-09-01 21:38:29 -0300

    Aula 1 Ex-1

--- Bloco1Java-dia1/src/Familia32/Dia1Java.java
+++ Bloco1Java-dia1/src/Familia32/Dia1Java.java
@@ -1,9 +1,34 @@
 package Familia32;
 
+import java.util.Scanner;
+
 public class Dia1Java {
 
 	public static void main(String[] args) {
 		
+		int a,b,c,Maior=0;
+		Scanner leia = new Scanner(System.in);
+		
+	System.out.println("\nReceba o valor de a: ");
+	a = leia.nextInt();
+	System.out.println("\nReceba o valor de b: ");
+	b= leia.nextInt();
+	System.out.println("\nReceba o valor de c: ");
+	c = leia.nextInt();
+	
+	if(a>Maior) {
+		Maior = a;					
+	}
+	
+	if(b>Maior) {
+		Maior = b;					
+	}
+	
+	if(c>Maior) {
+		Maior = c;					
+	}
+		
+	System.out.print("\nO maior numero é: " + Maior);
 
 	}
 
